3ddd74affe2d1f975bc0cc5f0591e0a5a1e166d7
Cen Wang <cenwang@umass.edu>
2023-04-08 03:35:50 +0000

    Add user display name

--- configuration.nix
+++ configuration.nix
@@ -63,6 +63,7 @@
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.derekwang = {
     isNormalUser = true;
+    description = "Derek Wang";
     extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
     packages = with pkgs; [
       firefox
